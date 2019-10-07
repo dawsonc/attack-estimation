a11dc3818f747b84ca5ebfb4f422b83c19a575d2
Mycal Tucker <mycal@mit.edu>
2019-10-07 09:22:23 -0400

    New feature calculates joint probability.
    Given a complete set of variables and their values, calculates the
    probability of that assignment.

--- bayes_net.py
+++ bayes_net.py
@@ -50,6 +50,16 @@ class BayesNode:
         for parent_node, val in parent_vals:
             assert val in parent_node.domain, "Couldn't find " + str(val) + " in parent node " + str(parent_node) + " domain."
         self.conditional_distribution[conditioning_event] = distribution
+        
+    def get_prob_value(self, val, parent_vals=None):
+        if parent_vals is None:
+            assert not self.parents, "Cannot ask for probability without giving parent values"
+        if not self.parents:
+            return self.marginal_distribution.get(val)
+        # Turn the parent assignments into a set.
+        conditioning_event = frozenset(parent_vals)
+        return self.conditional_distribution.get(conditioning_event).get(val)
+        
 
 """
 Defines a BayesNet object. Just a holder for a bunch of nodes.
@@ -65,11 +75,31 @@ class BayesNet:
 
         # This would also be a great place to do sanity checks on the distributions themselves fully specifying
         # the distributions and summing to 1.
+        # Could also come up with some sort of topographical ordering (and check that there are no loops)
         
     def get_node(self, name):
         assert name in self.name_to_nodes.keys(), "Could not find node named " + name
         return self.name_to_nodes.get(name)
     
+    # Given assignments for all the variables, return the probability of that assignment
+    def calc_joint(self, var_assignments):
+        assert len(var_assignments) == len(self.nodes)
+        running_prob = 1.0
+        for var, value in var_assignments:
+            # Loop up the corresponding probability, which means I also need to check parents and their values.
+            if not var.parents:
+                marginal_prob = var.get_prob_value(value)
+                running_prob = running_prob * marginal_prob
+                continue
+            # Must look up parents and their values.
+            parent_assignments = []
+            for possible_var, possible_val in var_assignments:
+                if possible_var in var.parents:
+                    parent_assignments.append((possible_var, possible_val))
+            fetched_prob = var.get_prob_value(value, parent_assignments)
+            running_prob = running_prob * fetched_prob
+        return running_prob
+
     def draw_net(self):
         nxg = nx.DiGraph()
         edges = []
